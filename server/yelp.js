4e0d2b4ba17449ba2b3c96af341fb8e31fb464a4
Franklin Griffin <griffinf3@yahoo.com>
2018-01-22 12:17:29 -0500

    Add files via upload
    Secrets removed from configuration files.

--- server/yelp.js
+++ server/yelp.js
@@ -3,8 +3,8 @@
 module.exports = {
 // Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-'clientId': 'APpm8ZusqUevKtE_S9AMvw',
-'clientSecret': 'j7wj0RnVe2FC0Iz4Aef0mn3pgkJaJpc8ph0YGtSLXHvNUhGhz8h2K3iXBAd6NwMz',
+'clientId': 'ClientIdGoesHere',
+'clientSecret': 'clientSecretGoesHere',
 'searchRequest': {
   term:'greek',
   categories: 'restaurant',
